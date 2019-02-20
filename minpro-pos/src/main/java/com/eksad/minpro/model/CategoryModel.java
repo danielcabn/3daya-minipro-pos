9f8c32be9a962f791f6669f86a3f624e235c6f9f
nilchotynabilla <nilchotynabilla@gmail.com>
2019-02-20 18:09:31 +0700

    category model

---
+++ minpro-pos/src/main/java/com/eksad/minpro/model/CategoryModel.java
@@ -0,0 +1,101 @@
+package com.eksad.minpro.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="pos_mst_category")
+
+public class CategoryModel {
+	@Id
+	@Column(name="ID", columnDefinition="serial")
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "pos_mst_category")
+	@TableGenerator(name = "pos_mst_category_seq", table = "tbl_squance", 
+	pkColumnName = "seq_id", valueColumnName = "seq_value",
+	initialValue = 0, allocationSize=1)
+	private Integer id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="created_by")
+	private Integer createdBy;
+	
+	@Column(name="created_on")
+	private Date createdOn;
+	
+	@Column(name="modified_by")
+	private Integer modifiedBy;
+	
+	@Column(name="modified_on")
+	private Date modifiedOn;
+	
+	@Column(name="active")
+	private Boolean active;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(String createdOn) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date co = null;
+		try {
+			co = format.parse(createdOn); 
+		} catch (Exception e) {
+			this.createdOn= null;
+		}
+		this.createdOn=co;
+	}
+	public Integer getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(Integer modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void ModifiedOn(String modifiedOn) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date mo = null;
+		try {
+			mo = format.parse(modifiedOn); 
+			} catch (Exception e) {
+				this.modifiedOn= null;
+			}
+		this.modifiedOn=mo;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+}
