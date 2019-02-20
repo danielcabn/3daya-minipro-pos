05b7c4e8deabd0aadb8736c99d790e68fd638573
Daniel Cristofer Antonius <danielcristoferabn@gmail.com>
2019-02-20 17:23:22 +0700

    Employee Model Update

--- minpro-pos/src/main/java/com/eksad/minpro/model/EmployeeModel.java
+++ minpro-pos/src/main/java/com/eksad/minpro/model/EmployeeModel.java
@@ -1,5 +1,132 @@
 package com.eksad.minpro.model;
 
-public class EmployeeModel {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="pos_mst_employee")
+public class EmployeeModel {
+	@Id
+	@Column(name="ID", columnDefinition="serial")
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "pos_mst_employee_seq")
+	@TableGenerator(name = "pos_mst_employee_seq", table = "tbl_squence", 
+	pkColumnName = "seq_id", valueColumnName = "seq_value", 
+	initialValue = 0, allocationSize=1)
+	private Integer id;
+
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String nameName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="have_account")
+	private Boolean haveAccount;
+	
+	@Column(name="created_by")
+	private Integer createdBy;
+	
+	@Column(name="created_on")
+	private Date createdOn;
+	
+	@Column(name="modified_by")
+	private Integer modifiedBy;
+	
+	@Column(name="modified_on")
+	private Date modifiedOn;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getNameName() {
+		return nameName;
+	}
+
+	public void setNameName(String nameName) {
+		this.nameName = nameName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Boolean getHaveAccount() {
+		return haveAccount;
+	}
+
+	public void setHaveAccount(Boolean haveAccount) {
+		this.haveAccount = haveAccount;
+	}
+
+	public Integer getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Integer createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public Integer getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(Integer modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	
+	
 }
