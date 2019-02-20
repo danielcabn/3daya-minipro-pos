fcaca96846ad387c722e5151aea42a1efc11d952
nilchotynabilla <nilchotynabilla@gmail.com>
2019-02-21 00:26:31 +0700

    list category

---
+++ minpro-pos/src/main/java/com/eksad/minpro/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.eksad.minpro.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eksad.minpro.dao.CategoryDao;
+import com.eksad.minpro.model.CategoryModel;
+import com.eksad.minpro.service.CategoryService;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+	@Autowired
+	private CategoryDao dao;
+	
+	@Override
+	public List<CategoryModel> getList() {
+		return this.dao.getList();
+	}
+
+	@Override
+	public CategoryModel getById(Integer id) {
+		return this.dao.getById(id);
+	}
+
+	@Override
+	public void insert(CategoryModel model) {
+		this.dao.insert(model);
+	}
+
+	@Override
+	public void update(CategoryModel model) {
+		this.dao.update(model);
+	}
+
+	@Override
+	public void delete(CategoryModel model) {
+		this.dao.delete(model);
+	}
+	
+	//bagian dari API
+	@Override
+	public List<CategoryModel> search(String key) {
+		return this.dao.search(key);
+	}
+	
+}
